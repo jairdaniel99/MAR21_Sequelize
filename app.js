7d2b05dbc4214fc07ba7f1498acfc438746ab5fd
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-03-20 20:04:27 -0600

    used mysql2 to fetch students

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 // import express
 const express = require("express");
+// import mysql2
+const mysql = require("mysql2");
+// import config
+const config = require("./config");
+
+// create a connection to the database
+const pool = mysql.createPool(config.database);
 
 // create an instance of express server
 const app = express();
 
-app.get("/welcome", function (req, res) {
-  res.send("Welcome to express server!");
+app.get("/students", function (req, res) {
+  // get students from the DATABASE
+  pool.query("select * from students", (error, results, fields) => {
+    if (error) throw error;
+    // send students as a response
+    res.send(results);
+  });
 });
 
 // listen for requests on port 3000
