7757a5f189da94c76f28e43e33ef6d8d55cc4e50
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-03-26 18:55:42 -0600

    modularized student routes

--- app.js
+++ app.js
@@ -2,30 +2,33 @@
 const express = require("express");
 // import created sequelize instance
 const sequelize = require("./config");
-
 // import student model
 const Student = require("./models/student");
+// import student routes
+const studentRoutes = require("./routes/student");
 
 // test the connection to the database
 sequelize
-  .sync() // sync() will create the table if it doesn't exist
-  .then(() => console.log("connected"))
+  .sync() // sync() will create the table if it does not exist
+  .then(() => console.log("connected to database!"))
   .catch((error) => console.log(error));
 
 // create an instance of express server
 const app = express();
 
-app.get("/students", function (req, res) {
-  Student.findAll()
-    .then((students) => {
-      res.send(students);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+// json data middleware
+app.use(express.json());
+// form data middleware
+app.use(express.urlencoded({ extended: true }));
 
-// listen for requests on port 3000
-app.listen(3000, function () {
+//use student routes
+app.use(studentRoutes);
+
+// Process is an object that represents the current nodeJS process
+// process.env is an object that contains the current environment variables
+//process.env.PORT is an environment that is set by the hosting service
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running on port 3000");
 });
+
+//REFERENCE FOR CAPSTONE
---
+++ routes/student.js
@@ -0,0 +1,89 @@
+const Student = require("../models/student");
+// create a router instance/object to define our routes
+const router = require("express").Router();
+
+// get all students
+router.get("/students", function (req, res) {
+  //   get students from the database
+  Student.findAll()
+    .then((students) => {
+      res.send(students);
+    })
+    .catch((error) => {
+      console.log(error, 500);
+    });
+});
+
+// get a student by id
+router.get("/students/:id", function (req, res) {
+  // find the student with given id
+  Student.findByPk(parseInt(req.params.id))
+    .then((result) => {
+      // if the result is a truthy value
+      if (result) {
+        res.send(result);
+      } else {
+        res.send("Student not found.", 404);
+      }
+    })
+    .catch((error) => {
+      console.log(error, 500); // internal server error
+    });
+});
+
+// create student data to the database
+router.post("/students", function (req, res) {
+  // create a new student object
+  const newStudent = {
+    name: req.body.name,
+    grade: req.body.grade,
+    age: req.body.age,
+  };
+  // create a new student record in the database
+  Student.create(newStudent)
+    .then((student) => {
+      res.send(student);
+    })
+    .catch((error) => {
+      console.log(error, 500);
+    });
+});
+
+router.patch("/students/:id", function (req, res) {
+  // find the student with given id
+  // select * from students where id = *
+  Student.findByPk(parseInt(req.params.id))
+    .then((student) => {
+      student.name = req.body.name;
+      student.grade = req.body.grade;
+      student.age = req.body.age;
+      // save the updates
+      student.save();
+      req.send(student);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.delete("/students/:id", function (req, res) {
+  // find the student with given id
+  // select * from students where id = *
+  Student.findByPk(parseInt(req.params.id))
+    .then((result) => {
+      // if the result is a truthy value
+      if (result) {
+        // delete the student from the database
+        student.destroy();
+        res.send(student);
+      } else {
+        res.send("Student not found.", 404);
+      }
+    })
+    .catch((error) => {
+      console.log(error, 500);
+    });
+});
+
+// use this in router.js
+module.exports = router;
